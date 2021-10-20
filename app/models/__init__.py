c32aaaaa2f4e9445aad8533459cab43edcee1544
fanny-chan <85082899+fanny-chan@users.noreply.github.com>
2021-10-20 13:13:40 -0400

    created cart_items and fixed migrations

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,4 @@ from .user import User
 from .product import Product
 from .cart import Cart, cart_products
 from .review import Review
+from .cart_items import CartItem
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,10 +4,16 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
+        first_name='Demo',
+        last_name='lition',      
         username='Demo', email='demo@aa.io', password='password')
     marnie = User(
+        first_name='marnie',
+        last_name='lition',
         username='marnie', email='marnie@aa.io', password='password')
     bobbie = User(
+        first_name='bobbie',
+        last_name='lition',
         username='bobbie', email='bobbie@aa.io', password='password')
 
     db.session.add(demo)
