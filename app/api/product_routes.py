6724939a4d5f343d5739ef7a86deb7fb6b02a5ef
fanny-chan <85082899+fanny-chan@users.noreply.github.com>
2021-10-19 20:51:53 -0400

    routes for cart

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -3,12 +3,12 @@ from app.models import db
 from app.models.product import Product
 
 
-product_routes = Blueprint('boards', __name__, url_prefix='/products')
+product_routes = Blueprint('products', __name__, url_prefix='/products')
 
 # get all products
 
 @product_routes.route('/')
-def get_products();
+def get_products():
     products = Product.query.all()
 
     return {product.id:product.to_dict() for product in products}
