2623d5c94ce884e86ed34d49b4f950d4241eb56d
fanny-chan <85082899+fanny-chan@users.noreply.github.com>
2021-10-21 21:38:29 -0400

    Update product.py

--- app/models/product.py
+++ app/models/product.py
@@ -1,4 +1,9 @@
 from .db import db
+<<<<<<< Updated upstream
+=======
+# from app.models.cart import cart_products
+from datetime import datetime
+>>>>>>> Stashed changes
 
 class Product(db.Model):
     __tablename__ = 'products'
@@ -8,10 +13,25 @@ class Product(db.Model):
     title = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Integer, nullable=False)
     description = db.Column(db.String(1000), nullable=False)
+    quantity_available = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime(), default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime(), default=datetime.utcnow)
 
 # relationships
 seller = db.relationship("User", back_populates="products")
 
+<<<<<<< Updated upstream
+=======
+    # many to many carts -- products
+    # carts = db.relationship(
+    #     "Cart",
+    #     secondary=cart_products,
+    #     back_populates="products"
+    # )
+
+    # one to many product-- reviews
+    reviews = db.relationship("Review", back_populates="product")
+>>>>>>> Stashed changes
 
 def to_dict(self):
     return {
@@ -19,7 +39,8 @@ def to_dict(self):
         'seller_id': self.user_id,
         'title': self.title,
         'price': self.price,
-        'description': self.description
+        'description': self.description,
+        'quantity_available': self.quantity_available
         
     }
     
