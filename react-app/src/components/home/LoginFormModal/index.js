1b81ecbf34b926ec430394f1d4079d45bc94013d
fanny-chan <85082899+fanny-chan@users.noreply.github.com>
2021-10-26 16:50:31 -0400

    login modal working

--- react-app/src/components/home/LoginFormModal/index.js
+++ react-app/src/components/home/LoginFormModal/index.js
@@ -7,7 +7,7 @@ function LoginFormModal() {
 
   return (
     <>
-      <button id="loginModal" className="login_button" onClick={() => setShowModal(true)}>Log In</button>
+      <button id="loginModal" className="login_button" onClick={() => setShowModal(true)}>Log In MODAl</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <LoginForm />
