d45e4ed74b58cdb11019ed2486b29fd2bf9b5b98
fanny-chan <85082899+fanny-chan@users.noreply.github.com>
2021-10-30 00:19:59 -0400

    finished bottom footer

--- react-app/src/components/ProductDisplay/ProductCard.js
+++ react-app/src/components/ProductDisplay/ProductCard.js
@@ -14,6 +14,7 @@ export default function ProductCard() {
     }, [dispatch])
 
     return (
+        <>
         <div className="product-card-container">      
             {productsSection.map((product) => (
                 <div className="product-card"> 
@@ -22,17 +23,30 @@ export default function ProductCard() {
                     </div>
                     <div className="product-title">
                         <NavLink className="product-title" to={`/products/${product.id}`}>{product?.title}</NavLink>
-                        {/* <p className="product-username">{product?.user?.username}</p>
-                        <p className="product-description">{product?.description}</p> 
-                        <p className="product-price">{'$'+ product?.price}</p>
-                        <p className="product-quantity">{product?.quantity_available}</p>
-                        <div style={{height:"30px"}}></div> */}
                     </div>
                 </div>
                 )
             )}
         
         </div>
+            <div className="what-is">
+            <h2>What is Craftsi?</h2>
+            </div>
+        <div className="craftsi">
+            <div className="community">
+                <h3>A wonderful community</h3>
+                <p>Craftsi is a global online marketplace, where people come together to make, sell, buy, and collect unique items. We’re also a community pushing for positive change for small businesses, people, and the planet. </p>
+            </div>
+            <div className="support">
+                <h3>Support independent creators </h3>
+                <p>There’s no Craftsi warehouse – just millions of people selling the things they love. We make the whole process easy, helping you connect directly with makers to find something extraordinary.</p>
+            </div>
+            <div className="Peace of mind">
+                <h3>Peace of mind</h3>
+                <p>Your privacy is the highest priority of our dedicated team. And if you ever need assistance, we are always ready to step in for support.</p>
+            </div>
+        </div>
+        </>
     )
 }
 
